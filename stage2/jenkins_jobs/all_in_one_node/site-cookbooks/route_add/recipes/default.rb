46c35ade70937af71e02727806072eb7e6c9ea43
Max Mazur <sirmax123@gmail.com>
2017-01-16 23:13:34 +0200

    add ssh key

--- stage2/jenkins_jobs/all_in_one_node/site-cookbooks/route_add/recipes/default.rb
+++ stage2/jenkins_jobs/all_in_one_node/site-cookbooks/route_add/recipes/default.rb
@@ -1,3 +1,19 @@
 route '10.0.1.0/24' do
   gateway '10.0.10.1'
 end
+
+directory '/root/.ssh' do
+  owner 'root'
+  group 'root'
+  mode '0400'
+  action :create
+end
+
+puts("#{node[:jenkins_slave_public_key]}")
+file '/root/.ssh/authorized_keys' do
+  content "#{node[:jenkins_slave_public_key]}"
+  mode '0644'
+  owner 'root'
+  group 'root'
+end
+
